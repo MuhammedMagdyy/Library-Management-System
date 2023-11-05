d6289d66dfe11d887ef1ff61186635e538402281
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-11-05 13:33:32 +0200

    feat: Book model
    - Created book model by adding the main columns to it.

---
+++ src/database/prisma/migrations/20231105111952_book_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "availableQuantity" INTEGER NOT NULL DEFAULT 0,
+    "shelfLocation" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
