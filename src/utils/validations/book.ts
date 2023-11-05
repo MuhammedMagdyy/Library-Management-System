512aa14c9beb91bd63decf86a04d4660dabd5f40
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-11-05 16:56:08 +0200

    feat: Book validation
    - Applied validation using zod on all CRUD operations for book model.

---
+++ src/utils/validations/book.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const createBookSchema = z.object({
+  title: z.string().min(1).max(255).trim(),
+  author: z.string().min(1).max(255).trim(),
+  isbn: z.string().min(1).max(255).trim(),
+  availableQuantity: z.coerce.number().int().min(0),
+  shelfLocation: z.string().min(1).max(255).trim(),
+  image: z.string().trim(),
+});
+
+export const bookIdParamSchema = z.object({
+  id: z.coerce.number().int(),
+});
+
+export const updateBookSchema = z.object({
+  title: z.string().min(1).max(255).trim().optional(),
+  author: z.string().min(1).max(255).trim().optional(),
+  isbn: z.string().min(1).max(255).trim().optional(),
+  availableQuantity: z.coerce.number().int().min(0).optional(),
+  shelfLocation: z.string().min(1).max(255).trim().optional(),
+  image: z.string().trim().optional(),
+});
