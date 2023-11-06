8b96f8c375e08ccf8904a2c2785e1fce50dc2ac4
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-11-06 22:59:55 +0200

    feat: user model
    - Created user model.
    - Created enum for roles.

---
+++ src/database/prisma/migrations/20231106171750_user_model/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'BORROWER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'BORROWER',
+    "registeredDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
