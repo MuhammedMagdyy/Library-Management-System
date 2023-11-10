f816b9bc5693b6326f245b426e68e35768d42dc1
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-11-10 17:58:13 +0200

    refactor: routes
    - Added new base endpoints and applied isAuth middleware to secure endpoints.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import bookRouter from './bookRouter';
+import borrowerRouter from './borrowerRouter';
+import authRouter from './authRouter';
+import { isAuth } from '../middlewares';
 
 const router = Router();
 
+router.use('/auth', authRouter);
+router.use(isAuth);
 router.use('/books', bookRouter);
+router.use('/borrowers', borrowerRouter);
 
 export default router;
