30a8d54445b6d5caa223b4e1c6792913e7fe49c4
MuhammedMagdyy <mohamedmody04@gmail.com>
2023-11-10 18:01:52 +0200

    feat: borrower router
    - Applied HTTP methods [GET, PUT, DELETE] with validation and logic.

---
+++ src/routes/borrowerRouter.ts
@@ -0,0 +1,94 @@
+import { Router } from 'express';
+import { asyncWrapper } from '../core';
+import { userService } from '../modules';
+import { responseStatus } from '../helpers';
+import { borrowerIdParamSchema, updateBorrowerSchema } from '../utils';
+import { ApiError } from '../middlewares';
+
+const router = Router();
+
+router.get(
+  '/',
+  asyncWrapper(async (_req, res) => {
+    const borrowers = await userService.findMany();
+
+    res.status(200).json({
+      status: responseStatus.SUCCESS,
+      message: 'Borrowers fetched successfully',
+      data: borrowers,
+    });
+  }),
+);
+
+router.get(
+  '/:id',
+  asyncWrapper(async (req, res, next) => {
+    const { id } = borrowerIdParamSchema.parse(req.params);
+    const borrower = await userService.findOne(
+      { id },
+      {
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          role: true,
+          registeredDate: true,
+        },
+      },
+    );
+
+    if (!borrower) {
+      return next(new ApiError('Borrower not found', 404));
+    }
+
+    res.status(200).json({
+      status: responseStatus.SUCCESS,
+      message: 'Borrower fetched successfully',
+      data: borrower,
+    });
+  }),
+);
+
+router.put(
+  '/:id',
+  asyncWrapper(async (req, res, next) => {
+    const { id } = borrowerIdParamSchema.parse(req.params);
+    const borrower = await userService.findOne({ id });
+
+    if (!borrower) {
+      return next(new ApiError('Borrower not found', 404));
+    }
+
+    const borrowerSchema = updateBorrowerSchema.parse(req.body);
+    const updatedBorrower = await userService.updateOne({ id }, borrowerSchema);
+
+    res.status(200).json({
+      status: responseStatus.SUCCESS,
+      message: 'Borrower updated successfully',
+      data: {
+        id: updatedBorrower.id,
+        email: updatedBorrower.email,
+        name: updatedBorrower.name,
+        role: updatedBorrower.role,
+      },
+    });
+  }),
+);
+
+router.delete(
+  '/:id',
+  asyncWrapper(async (req, res, next) => {
+    const { id } = borrowerIdParamSchema.parse(req.params);
+    const borrower = await userService.findOne({ id });
+
+    if (!borrower) {
+      return next(new ApiError('Borrower not found', 404));
+    }
+
+    await userService.deleteOne({ id });
+
+    res.status(204).send();
+  }),
+);
+
+export default router;
